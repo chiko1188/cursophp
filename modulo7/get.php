354b146781e0947c8833b8c78c4a2a6e80fe1373
chiko <chikoterrazas@gmail.com>
2017-05-30 22:38:48 -0600

    get

---
+++ modulo7/get.php
@@ -0,0 +1,9 @@
+<form action="actionGet.php" method="get">
+  Name: <input type="text" name="name" /><br /><br />
+  Age: <input type="text" name="age" /><br /><br />
+  <input type="submit" name="submit" value="Submit" />
+</form>
+<?php
+echo "Hi ".$_GET['name'].". ";
+echo "You are ".$_GET['age']." years old.";
+?>
